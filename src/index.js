6a38d98f800a63883548f456c6af95eb13b55f3c
Evgeny Stepanov <evge.stepanov@mail.ru>
2024-07-01 00:10:17 +0500

    Working on auth.html

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import "./index.html";
 import "./auth.html";
 import "./index.scss";
 
-import example from "./img/img-png.png";
+//import example from "./img/img-png.png";
 
 import { mult, sum } from "./modules/calc";
 
-const imgWrap = document.querySelector(".img");
-const img = new Image();
-img.src = example;
-imgWrap.append(img);
+//const imgWrap = document.querySelector(".img");
+//const img = new Image();
+//img.src = example;
+//imgWrap.append(img);
 
 console.log(sum(5, 5));
 console.log(mult(5, 5));
+
